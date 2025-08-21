5d5e86f7e5b62c57ef98450d2fa8dbd81ba3a9c9
ericperske <eric.perske@web.de>
2025-08-21 16:34:31 +0200

    added first examples

---
+++ test_projects/scatter_test.py
@@ -0,0 +1,38 @@
+from dash import Dash, dcc, html, callback, Output, Input
+import plotly.express as px
+import pandas as pd
+
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminderDataFiveYear.csv')
+
+app = Dash()
+
+fig = px.scatter(df, x='gdpPercap', y='lifeExp', size='pop',
+                 color='continent', hover_name='country', log_x=True, size_max=60)
+
+app.layout = html.Div([
+    dcc.Graph(id='graph-with-slider'),
+    dcc.Slider(
+        df['year'].min(),
+        df['year'].max(),
+        step=None,
+        value=df['year'].min(),
+        marks={str(year): str(year) for year in df['year'].unique()},
+        id='year-slider'
+    )
+])
+
+@callback(
+    Output('graph-with-slider', 'figure'),
+    Input('year-slider', 'value')
+)
+def update_figure(selected_year):
+    filtered_df = df[df.year == selected_year]
+
+    fig = px.scatter(filtered_df, x="gdpPercap", y="lifeExp",
+                     size="pop", color="continent", hover_name='country',
+                     log_x=True, size_max=55)
+    fig.update_layout(transition_duration=300)
+    return fig
+
+
+app.run()
